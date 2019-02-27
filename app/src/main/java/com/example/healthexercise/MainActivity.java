5e038ded0399ac18a4e651beab9b5d8f34d4eeb1
andreasp1997 <andreasp97xhd@gmail.com>
2019-02-27 18:59:31 +0100

    Progress circle for distance walked/jogged

--- app/src/main/java/com/example/healthexercise/MainActivity.java
+++ app/src/main/java/com/example/healthexercise/MainActivity.java
@@ -97,9 +97,21 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+
+            Fragment fragment = null;
+            switch (position) {
+                case 0:
+                    fragment = new ExerciseFragment();
+                    break;
+                case 1:
+                    fragment = new HomeFragment();
+                    break;
+                case 2:
+                    fragment = new DietFragment();
+                    break;
+            }
+            return fragment;
+
         }
 
         @Override
