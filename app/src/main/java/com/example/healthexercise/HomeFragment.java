16a18d7a87da5f88c4517b5eaabd5832741f3088
andreasp1997 <andreasp97xhd@gmail.com>
2019-03-04 19:51:59 +0100

    Step counter working (arcprogress needs fixing)

--- app/src/main/java/com/example/healthexercise/HomeFragment.java
+++ app/src/main/java/com/example/healthexercise/HomeFragment.java
@@ -1,8 +1,10 @@
 package com.example.healthexercise;
 
+import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,35 +13,77 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-//import com.github.lzyzsd.circleprogress.ArcProgress;
+import com.github.lzyzsd.circleprogress.ArcProgress;
+
+import static android.content.Context.SENSOR_SERVICE;
 
 public class HomeFragment extends Fragment implements SensorEventListener, StepListener {
 
-   //private ArcProgress arcProgress;
+   private ArcProgress arcProgress;
+
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private int numSteps;
+    Thread t1;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.home_frag, container, false);
 
-       //arcProgress = (ArcProgress) v.findViewById(R.id.distance_progress);
-       //arcProgress.setSuffixText("");
+        sensorManager = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new StepDetector();
+        simpleStepDetector.registerListener(this);
+
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+
+        arcProgress =(ArcProgress)v.findViewById(R.id.distance_progress);
+
+        t1 = new Thread() {
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                arcProgress.setSuffixText(String.valueOf(numSteps));
+
+                            }
+                        });//runOnUiThread ends here
+                    }//While ends here
+                } catch (InterruptedException e) {
+                }//Catch ends here
+            }
+
+        };
+
+        t1.start();
 
         return v;
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
+
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
 
     @Override
     public void step(long timeNS) {
+
+        numSteps++;
 
     }
 }
--- app/src/main/java/com/example/healthexercise/RegisterActivity.java
+++ app/src/main/java/com/example/healthexercise/RegisterActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
