521755a8cc9c55a54c9cb5fa9df5ef1a91c445dc
andreasp1997 <andreasp97xhd@gmail.com>
2019-03-12 19:32:23 +0100

    Design update

--- app/src/main/java/com/example/healthexercise/ExerciseFragment.java
+++ app/src/main/java/com/example/healthexercise/ExerciseFragment.java
@@ -8,11 +8,11 @@ import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 
-import com.google.android.gms.common.ConnectionResult;
+
 import android.location.LocationListener;
 
 import android.location.LocationManager;
-import android.os.Build;
+
 import android.os.Bundle;
 
 import android.provider.Settings;
@@ -22,25 +22,20 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
