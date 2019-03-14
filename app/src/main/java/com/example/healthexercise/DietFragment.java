d7bf817accf0fcbfb9cace76dfd8245559982295
andreasp1997 <andreasp97xhd@gmail.com>
2019-03-14 02:57:24 +0100

    Add meal dialog added

--- app/src/main/java/com/example/healthexercise/DietFragment.java
+++ app/src/main/java/com/example/healthexercise/DietFragment.java
@@ -1,14 +1,20 @@
 package com.example.healthexercise;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,6 +23,8 @@ import java.util.List;
 public class DietFragment extends Fragment {
 
     ListView listView;
+    FloatingActionButton addBtn;
+
 
     @Nullable
     @Override
@@ -24,6 +32,14 @@ public class DietFragment extends Fragment {
         View v = inflater.inflate(R.layout.diet_frag, container, false);
 
         listView = (ListView) v.findViewById(R.id.list);
+
+        addBtn = v.findViewById(R.id.floatingActionButton);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialogDiet();
+            }
+        });
 
         String[] days = new String[] {
                 "Monday",
@@ -42,5 +58,34 @@ public class DietFragment extends Fragment {
         listView.setAdapter(adapter);
 
         return v;
+    }
+
+    private void openDialogDiet(){
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View subView = inflater.inflate(R.layout.custom_dialog_3, null);
+        final TimePicker timePicker = subView.findViewById(R.id.time_picker);
+        timePicker.setIs24HourView(true);
+        final EditText infoBox = (EditText)subView.findViewById(R.id.info_box);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.MyDialogTheme);
+        builder.setTitle("Enter Meal For Diet");
+        builder.setView(subView);
+        AlertDialog alertDialog = builder.create();
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "Meal added to diet", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "Cancel", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        builder.show();
     }
 }
