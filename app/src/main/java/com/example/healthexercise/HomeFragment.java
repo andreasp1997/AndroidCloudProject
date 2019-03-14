a98fee2c48f2c32be87af31138657672af646afd
andreasp1997 <andreasp97xhd@gmail.com>
2019-03-14 22:27:16 +0100

    alert dialog weight: get calories needed to maintain weight

--- app/src/main/java/com/example/healthexercise/HomeFragment.java
+++ app/src/main/java/com/example/healthexercise/HomeFragment.java
@@ -12,10 +12,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,6 +29,7 @@ import android.widget.Toast;
 
 import com.github.lzyzsd.circleprogress.ArcProgress;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +43,8 @@ public class HomeFragment extends Fragment implements SensorEventListener, StepL
     private SensorManager sensorManager;
     private Sensor accel;
     private int numSteps;
+    private DecimalFormat df;
+    private Double neededCalories;
     Thread t1;
 
     private ImageButton editStep;
@@ -160,12 +165,14 @@ public class HomeFragment extends Fragment implements SensorEventListener, StepL
     private void openDialogCalories(){
         LayoutInflater inflater = LayoutInflater.from(getActivity());
 
+        df = new DecimalFormat("###.###");
+
         View subView = inflater.inflate(R.layout.custom_dialog_2, null);
 
         final EditText editWeight = (EditText)subView.findViewById(R.id.edit_weight);
         final EditText editHeight = (EditText)subView.findViewById(R.id.edit_height);
 
-        List<String> spinnerArray =  new ArrayList<String>();
+        final List<String> spinnerArray =  new ArrayList<String>();
         spinnerArray.add("Male");
         spinnerArray.add("Female");
 
@@ -173,7 +180,7 @@ public class HomeFragment extends Fragment implements SensorEventListener, StepL
                 android.R.layout.simple_spinner_item, spinnerArray);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner sItems = (Spinner) subView.findViewById(R.id.spinner);
+        final Spinner sItems = (Spinner) subView.findViewById(R.id.spinner);
         sItems.setAdapter(adapter);
 
         final EditText editAge = (EditText)subView.findViewById(R.id.edit_age);
@@ -181,6 +188,100 @@ public class HomeFragment extends Fragment implements SensorEventListener, StepL
         final EditText editCalories = (EditText)subView.findViewById(R.id.edit_calories);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.MyDialogTheme);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if (editWeight.getText().toString().trim().isEmpty() || editHeight.getText().toString().isEmpty()
+                        || editAge.getText().toString().isEmpty()) {
+
+                    maintainWeightText.setText("Leave no above fields empty");
+                }
+
+                else {
+
+                    if (sItems.getSelectedItem().equals("Male")){
+
+                        neededCalories = 66.4730 + (13.7516 *
+                                Integer.parseInt(editWeight.getText().toString())) +
+                                (5.0033 * Integer.parseInt(editHeight.getText().toString()))
+                                - (6.7550 * Integer.parseInt(editAge.getText().toString()));
+
+
+                        maintainWeightText.setText(String.valueOf(df.format(neededCalories)) + " " +
+                                "calories a day to maintain weight");
+
+                    } else if (sItems.getSelectedItem().equals("Female")){
+
+                        neededCalories = 655.0955 + (9.5634 *
+                                Integer.parseInt(editWeight.getText().toString())) +
+                                (1.8496 * Integer.parseInt(editHeight.getText().toString()))
+                                - (4.6756 * Integer.parseInt(editAge.getText().toString()));
+
+                        maintainWeightText.setText(df.format(neededCalories) + " " +
+                                "calories a day to maintain weight");
+                    }
+                }
+            }
+        };
+
+        editWeight.addTextChangedListener(textWatcher);
+        editAge.addTextChangedListener(textWatcher);
+        editHeight.addTextChangedListener(textWatcher);
+
+        sItems.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if (editWeight.getText().toString().trim().isEmpty() || editHeight.getText().toString().isEmpty()
+                        || editAge.getText().toString().isEmpty()) {
+
+                    maintainWeightText.setText("Leave no above fields empty");
+                }
+
+                else {
+
+                    if (sItems.getSelectedItem().equals("Male")){
+
+                        neededCalories = 66.4730 + (13.7516 *
+                                Integer.parseInt(editWeight.getText().toString())) +
+                                (5.0033 * Integer.parseInt(editHeight.getText().toString()))
+                                - (6.7550 * Integer.parseInt(editAge.getText().toString()));
+
+
+                        maintainWeightText.setText(String.valueOf(df.format(neededCalories)) + " " +
+                                "calories a day to maintain weight");
+
+                    } else if (sItems.getSelectedItem().equals("Female")){
+
+                        neededCalories = 655.0955 + (9.5634 *
+                                Integer.parseInt(editWeight.getText().toString())) +
+                                (1.8496 * Integer.parseInt(editHeight.getText().toString()))
+                                - (4.6756 * Integer.parseInt(editAge.getText().toString()));
+
+                        maintainWeightText.setText(df.format(neededCalories) + " " +
+                                "calories a day to maintain weight");
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         builder.setTitle("Set Calorie Intake");
         builder.setMessage("Enter your weight, height, gender and age to calculate calories needed to maintain weight" +
                 " then enter your desired calorie intake");
@@ -202,5 +303,7 @@ public class HomeFragment extends Fragment implements SensorEventListener, StepL
         });
 
         builder.show();
+
+
     }
 }
